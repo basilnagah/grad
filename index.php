fde65e3bbc62e5cccd4c10b1f0006b76c79c53c7
basilnagah <115904109+basilnagah@users.noreply.github.com>
2025-04-24 23:49:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 
+<?php
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/':
+        echo "Welcome to the home page!";
+        break;
+    case '/about':
+        echo "This is the about page.";
+        break;
+    default:
+        http_response_code(404);
+        echo "404 - Page not found";
+        break;
+}
