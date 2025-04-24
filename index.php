e038162383b4b470b93b5c7c73b520d01a46800b
basilnagah <115904109+basilnagah@users.noreply.github.com>
2025-04-25 02:01:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
-
 <?php
-$request = $_SERVER['REQUEST_URI'];
-
-switch ($request) {
-    case '/':
-        echo "Welcome to the home page!";
-        break;
-    case '/about':
-        echo "This is the about page.";
-        break;
-    default:
-        http_response_code(404);
-        echo "404 - Page not found";
-        break;
-}
+echo "API is running.";
