da3de86e615d031e7a9a496fa9a5b3f37bd8d85f
basilnagah <115904109+basilnagah@users.noreply.github.com>
2025-04-24 23:56:38 +0200

    Update connection.php

--- conn/connection.php
+++ conn/connection.php
@@ -1,10 +1,14 @@
-<?php 
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "emall"; 
+<?php
+$servername = "shinkansen.proxy.rlwy.net";
+$username = "root";
+$password = "vtEYOwzOZYCNwFqkaEqfEowwYWKKlmUr";
+$dbname = "railway";
+$port = 17416;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
